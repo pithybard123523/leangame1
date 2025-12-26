3e9b2a6af3f13043a3dc44654526899abe3311c2
codingq2 <3063823518@qq.com>
2025-12-26 03:01:06 +0000

    image

--- Game.lean
+++ Game.lean
@@ -1,4 +1,8 @@
 import Game.Levels.TutorialWorld
+import Game.Levels.AdditionWorld
+import Game.Levels.ScalarMulti
+import Game.Levels.MultiWorld
+import Game.Levels.NormWorld
 
 -- Here's what we'll put on the title screen
 Title "Vector Game"
@@ -15,10 +19,10 @@ lake version:4.2
 
 /-! Information to be displayed on the servers landing page. -/
 Languages "en"
-CaptionShort "Game Template"
-CaptionLong "You should use this game as a template for your own game and add your own levels."
--- Prerequisites "" -- add this if your game depends on other games
--- CoverImage "images/cover.png"
+CaptionShort "Vector Game"
+CaptionLong "A game to prove the vector laws."
+Prerequisites "NNG" -- add this if your game depends on other games
+CoverImage "images/vector_game_cover.png"
 
 /-! Build the game. Show's warnings if it found a problem with your game. -/
 MakeGame
