9714914d3e3a8361a074b750c42949d621ad1716
codingq2 <3063823518@qq.com>
2026-01-01 17:04:20 +0000

    try

--- Game.lean
+++ Game.lean
@@ -4,8 +4,8 @@ import GameServer.Commands
 import VectorGame.TutorialWorld
 import VectorGame.AdditionWorld
 import VectorGame.ScalarWorld
-import Game.Levels.MultiWorld
-import Game.Levels.NormWorld
+import VectorGame.MultiWorld
+import VectorGame.NormWorld
 
 Game "Vector Algebra Game" where
 introduction := "A step-by-step Lean4 proof game for 2D vector algebra"
